5350864855500916fa018f3157e0940ddf865320
Sssss-Mmm <elation4927@gmail.com>
2025-08-20 01:20:22 +0900

    feat: 프로필 조회 시 복약 기록 반환 및 DTO 구조 개선

--- src/dtos/auth/userResponseDto.js
+++ src/dtos/auth/userResponseDto.js
@@ -11,14 +11,16 @@ class UserResponseDto {
         this.residence = user.region;
         this.createdAt = user.created_at;
         this.diseases = user.Diseases ? user.Diseases.map(d => ({
-            id: d.disease_id,
-            name: d.disease_name
+            id: d.disease_id
         })) : []
-        this.user_medi_history = user.User_Medi_History ? user.User_Medi_History.map(m => ({
-            id: m.medi_id,
-            name: m.medi_name
+        this.history = user.User_Medi_History ? user.User_Medi_History.map(m => ({
+            name: m.medi_name,
+            start_date: m.start_date,
+            end_date: m.end_date,
+            status: m.status,
+            dosage: m.dosage
         })) : [];
-        this.userImg = user.user_img || null; // 기본값으로 null 설정
+        this.user_img = user.user_img || null; // 기본값으로 null 설정
     }
 }
 
